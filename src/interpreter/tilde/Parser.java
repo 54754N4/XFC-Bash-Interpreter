2dbab348fe2246a7a56e93c078e0d26f642399a2
54754N4 <35555527+54754N4@users.noreply.github.com>
2020-06-16 14:37:11 +0700

    Completed and tested tilde expansion

--- src/interpreter/tilde/Parser.java
+++ src/interpreter/tilde/Parser.java
@@ -1,18 +1,31 @@
 package interpreter.tilde;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import model.ConsoleContext;
+import model.user.User;
+
 public class Parser {
-	public static final String SEPARATORS = ": \t\n";
-	public static final char TILDE = '~';
 	
-	private String input;
-	
-	public Parser(String input) {
-		this.input = input;
+	public String parse(String input) {
+		String result = input, name, target, pwd, oldpwd, home;
+		Matcher m = Pattern.compile("(~([a-z\" ]+))").matcher(result);
+		if (m.matches()) {
+			target = m.group(1);
+			name = m.group(2);
+			if (name.startsWith("\"") && name.endsWith("\""))
+				name = name.substring(1, name.length()-1);
+			User user = User.retrieve(name);
+			if (user != null) 
+				result = result.replace(target, user.getHomeDirectory());
+		} if (result.contains("~+") && (pwd = ConsoleContext.INSTANCE.get(ConsoleContext.PWD)) != null) 
+			result = result.replace("~+", pwd);
+		if (result.contains("~-") && (oldpwd = ConsoleContext.INSTANCE.get(ConsoleContext.OLDPWD)) != null)
+			result = result.replace("~-", oldpwd);
+		if (result.contains("~") && (home = ConsoleContext.INSTANCE.get(ConsoleContext.HOME)) != null)
+			result = result.replaceAll("~(?!(\\+|-|[a-zA-Z]))", home);
+		return result;
 	}
 	
-	public String parse() {
-		for (int i=0; i<input.length(); i++) {
-			
-		}
-	}
 }
--- src/test/parsing/TestTildeParser.java
+++ src/test/parsing/TestTildeParser.java
@@ -1,7 +1,19 @@
 package test.parsing;
 
+import interpreter.tilde.Parser;
+
 public class TestTildeParser {
 	public static void main(String[] args) {
-		
+		String[] inputs = {
+			"~",
+			"~+",
+			"~-",
+			"~default",
+			"~\"default\"",
+			"PATH=~/mybins:~peter/mybins:$PATH"
+		};
+		Parser parser = new Parser();
+		for (String input : inputs)
+			System.out.println(parser.parse(input));
 	}
 }
