7810f16432dc2aa1bb2a3f1c9160168756202bc4
54754N4 <35555527+54754N4@users.noreply.github.com>
2020-06-17 04:19:23 +0700

    Extracted tokens from source

---
+++ BashTokens.c
@@ -0,0 +1,105 @@
+// TOKENS
+
+/* Reserved words.  Members of the first group are only recognized
+   in the case that they are preceded by a list_terminator.  Members
+   of the second group are for [[...]] commands.  Members of the
+   third group are recognized only under special circumstances. 
+1. IF THEN ELSE ELIF FI CASE ESAC FOR SELECT WHILE UNTIL DO DONE FUNCTION COPROC
+2. COND_START COND_END COND_ERROR
+3. IN BANG TIME TIMEOPT TIMEIGN
+*/
+
+/* More general tokens. yylex () knows how to make these. 
+<word> WORD ASSIGNMENT_WORD REDIR_WORD
+<number> NUMBER
+<word_list> ARITH_CMD ARITH_FOR_EXPRS
+<command> COND_CMD
+AND_AND OR_OR GREATER_GREATER LESS_LESS LESS_AND LESS_LESS_LESS
+GREATER_AND SEMI_SEMI SEMI_AND SEMI_SEMI_AND
+LESS_LESS_MINUS AND_GREATER AND_GREATER_GREATER LESS_GREATER
+GREATER_BAR BAR_AND
+*/
+
+// ~~~~ YYLEX part extracted from parse.y & y.tab.c ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+/* Reserved words.  These are only recognized as the first word of a
+   command. */
+STRING_INT_ALIST word_token_alist[] = {
+  { "if", IF },
+  { "then", THEN },
+  { "else", ELSE },
+  { "elif", ELIF },
+  { "fi", FI },
+  { "case", CASE },
+  { "esac", ESAC },
+  { "for", FOR },
+#if defined (SELECT_COMMAND)
+  { "select", SELECT },
+#endif
+  { "while", WHILE },
+  { "until", UNTIL },
+  { "do", DO },
+  { "done", DONE },
+  { "in", IN },
+  { "function", FUNCTION },
+#if defined (COMMAND_TIMING)
+  { "time", TIME },
+#endif
+  { "{", '{' },
+  { "}", '}' },
+  { "!", BANG },
+#if defined (COND_COMMAND)
+  { "[[", COND_START },
+  { "]]", COND_END },
+#endif
+#if defined (COPROCESS_SUPPORT)
+  { "coproc", COPROC },
+#endif
+  { (char *)NULL, 0}
+};
+
+/* other tokens that can be returned by read_token() */
+STRING_INT_ALIST other_token_alist[] = {
+  /* Multiple-character tokens with special values */
+  { "--", TIMEIGN },
+  { "-p", TIMEOPT },
+  { "&&", AND_AND },
+  { "||", OR_OR },
+  { ">>", GREATER_GREATER },
+  { "<<", LESS_LESS },
+  { "<&", LESS_AND },
+  { ">&", GREATER_AND },
+  { ";;", SEMI_SEMI },
+  { ";&", SEMI_AND },
+  { ";;&", SEMI_SEMI_AND },
+  { "<<-", LESS_LESS_MINUS },
+  { "<<<", LESS_LESS_LESS },
+  { "&>", AND_GREATER },
+  { "&>>", AND_GREATER_GREATER },
+  { "<>", LESS_GREATER },
+  { ">|", GREATER_BAR },
+  { "|&", BAR_AND },
+  { "EOF", yacc_EOF },
+  /* Tokens whose value is the character itself */
+  { ">", '>' },
+  { "<", '<' },
+  { "-", '-' },
+  { "{", '{' },
+  { "}", '}' },
+  { ";", ';' },
+  { "(", '(' },
+  { ")", ')' },
+  { "|", '|' },
+  { "&", '&' },
+  { "newline", '\n' },
+  { (char *)NULL, 0}
+};
+
+/* others not listed here:
+	WORD			look at yylval.word
+	ASSIGNMENT_WORD		look at yylval.word
+	NUMBER			look at yylval.number
+	ARITH_CMD		look at yylval.word_list
+	ARITH_FOR_EXPRS		look at yylval.word_list
+	COND_CMD		look at yylval.command
+*/
