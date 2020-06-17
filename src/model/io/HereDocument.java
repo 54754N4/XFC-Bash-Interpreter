82c26d675d28749cc6477f9995daf7035c3f146e
54754N4 <35555527+54754N4@users.noreply.github.com>
2020-06-17 15:03:28 +0700

    Added reference for heredoc testing

--- src/model/io/HereDocument.java
+++ src/model/io/HereDocument.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
+// For when u wanna test multiple IO :
+// https://linuxize.com/post/bash-heredoc/#:~:text=In%20Bash%20and%20other%20shells,of%20input%20to%20a%20command.&text=The%20first%20line%20starts%20with,%3C%3C%20and%20the%20delimiting%20identifier.
 public class HereDocument extends InputStream {
 	private byte[] bytes;
 	private int current;
