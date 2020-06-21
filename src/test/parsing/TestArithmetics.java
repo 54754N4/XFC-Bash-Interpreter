59dcc26f5cafff610ce5eb372ac8f2400244fa7d
54754N4 <35555527+54754N4@users.noreply.github.com>
2020-06-21 10:20:24 +0700

    Update TestArithmetics.java

--- src/test/parsing/TestArithmetics.java
+++ src/test/parsing/TestArithmetics.java
@@ -5,7 +5,7 @@ import interpreter.exceptions.ParsingException;
 
 public class TestArithmetics {
 	public static void main(String[] args) throws ParsingException {
-		interpreter.arithmetic.Interpreter interpreter = new Interpreter("2+3*4+PI+exp(ln(1))\n");
+		interpreter.arithmetic.Interpreter interpreter = new Interpreter("2+3*4+PI+exp(ln(1))\n+2**(1+2)");
 		System.out.println(interpreter.interpret());
 	}
 }
