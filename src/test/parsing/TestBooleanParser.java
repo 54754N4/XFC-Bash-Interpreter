2989278cf34de89bb61c21804ae1ba46ed376cd6
54754N4 <35555527+54754N4@users.noreply.github.com>
2020-06-22 10:16:43 +0700

    Added boolean expression tests

--- src/test/parsing/TestBooleanParser.java
+++ src/test/parsing/TestBooleanParser.java
@@ -8,17 +8,19 @@ import interpreter.exceptions.ParsingException;
 import interpreter.generic.Token;
 
 public class TestBooleanParser {
-	private static String input = "((-e filepath) -a ((strin1 == string2) == (-S socket.file)))";
+	public static String input = "-e filepath -eq -o (strin1 == string2) < -S socket.file",  
+		input0 = "-e filepath -eq (strin1 == \"some result\") > -S socket.file",
+		input1 = "-e filepath -eq (\"$file\" == $result) =~ -S socket.file";
 	
 	public static void testLexer() throws ParsingException {
-		BooleanLexer lexer = new BooleanLexer(input);
+		BooleanLexer lexer = new BooleanLexer(input1);
 		Token<Type> token;
 		while ((token = lexer.getNextToken()).type != Type.EOF)
 			System.out.println(token);
 	}
 	
 	public static void testParser() throws ParsingException {
-		BooleanLexer lexer = new BooleanLexer(input);
+		BooleanLexer lexer = new BooleanLexer(input1);
 		BooleanParser parser = new BooleanParser(lexer);
 		AST tree = parser.parse();
 		System.out.println(tree);
