878a8934cf3f82082bbd2288b4773bb9d257be0f
54754N4 <35555527+54754N4@users.noreply.github.com>
2020-06-23 04:46:05 +0700

    Added test case for boolean expression interpreter

--- src/test/parsing/TestBooleanParser.java
+++ src/test/parsing/TestBooleanParser.java
@@ -1,16 +1,21 @@
 package test.parsing;
 
+import interpreter.bool.BooleanInterpreter;
 import interpreter.bool.BooleanLexer;
 import interpreter.bool.BooleanParser;
 import interpreter.bool.Type;
 import interpreter.bool.ast.AST;
+import interpreter.exceptions.InterpretingException;
 import interpreter.exceptions.ParsingException;
 import interpreter.generic.Token;
+import model.ConsoleContext;
 
 public class TestBooleanParser {
 	public static String input = "-e filepath -eq -o (strin1 == string2) < -S socket.file",  
 		input0 = "-e filepath -eq (strin1 == \"some result\") > -S socket.file",
-		input1 = "-e filepath -eq (\"$file\" == $result) =~ -S socket.file";
+		input1 = "-e filepath -eq (\"$file\" == $result) =~ -S socket.file",
+		real = "BashTokens.c =~ Bash.* && -e BashTokens.c",
+		math = "1+2+3+4 -gt 0";
 	
 	public static void testLexer() throws ParsingException {
 		BooleanLexer lexer = new BooleanLexer(input1);
@@ -26,8 +31,16 @@ public class TestBooleanParser {
 		System.out.println(tree);
 	}
 	
-	public static void main(String[] args) throws ParsingException {
+	public static void testInterpreter() throws InterpretingException, ParsingException {
+		BooleanLexer lexer = new BooleanLexer(math);
+		BooleanParser parser = new BooleanParser(lexer);
+		BooleanInterpreter interpreter = new BooleanInterpreter(parser, ConsoleContext.INSTANCE);
+		System.out.println(interpreter.interpret());
+	}
+	
+	public static void main(String[] args) throws ParsingException, InterpretingException {
 //		testLexer();
-		testParser();
+//		testParser();
+		testInterpreter();
 	}
 }
