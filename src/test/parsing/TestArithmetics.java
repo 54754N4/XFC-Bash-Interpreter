f0de93391163178d225507147e2a63916680359e
54754N4 <35555527+54754N4@users.noreply.github.com>
2020-06-23 04:45:20 +0700

    Renamed to follow naming convention

--- src/test/parsing/TestArithmetics.java
+++ src/test/parsing/TestArithmetics.java
@@ -1,11 +1,11 @@
 package test.parsing;
 
-import interpreter.arithmetic.Interpreter;
+import interpreter.arithmetic.ArithmeticInterpreter;
 import interpreter.exceptions.ParsingException;
 
 public class TestArithmetics {
 	public static void main(String[] args) throws ParsingException {
-		interpreter.arithmetic.Interpreter interpreter = new Interpreter("2+3*4+PI+exp(ln(1))\n+2**(1+2)");
+		interpreter.arithmetic.ArithmeticInterpreter interpreter = new ArithmeticInterpreter("2+3*4+PI+exp(ln(1))\n+2**(1+2)");
 		System.out.println(interpreter.interpret());
 	}
 }
