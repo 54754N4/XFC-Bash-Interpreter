9d72e6e521f8391114cf75428b0c248bbbe2daab
54754N4 <35555527+54754N4@users.noreply.github.com>
2020-06-29 10:44:03 +0700

    Added new tests

--- src/test/parsing/TestBooleanParser.java
+++ src/test/parsing/TestBooleanParser.java
@@ -15,32 +15,38 @@ public class TestBooleanParser {
 		input0 = "-e filepath -eq (strin1 == \"some result\") > -S socket.file",
 		input1 = "-e filepath -eq (\"$file\" == $result) =~ -S socket.file",
 		real = "BashTokens.c =~ Bash.* && -e BashTokens.c",
-		math = "1+2+3+4 -eq 10";
+		real1 = "-e BashTokens.c && 1+2 -eq 10",
+		math = "1+2+3+4 -eq 10",
+		math1 = "1+2 -eq 4 &! 2+3 -ne 3 || 5 -lt 4";
 	
-	public static void testLexer() throws ParsingException {
-		BooleanLexer lexer = new BooleanLexer(input1);
+	public static void testLexer(String input) throws ParsingException {
+		BooleanLexer lexer = new BooleanLexer(input);
 		Token<Type> token;
 		while ((token = lexer.getNextToken()).type != Type.EOF)
 			System.out.println(token);
 	}
 	
-	public static void testParser() throws ParsingException {
-		BooleanLexer lexer = new BooleanLexer(input1);
+	public static void testParser(String input) throws ParsingException {
+		BooleanLexer lexer = new BooleanLexer(input);
 		BooleanParser parser = new BooleanParser(lexer);
 		AST tree = parser.parse();
 		System.out.println(tree);
 	}
 	
 	public static void testInterpreter() throws InterpretingException, ParsingException {
-		BooleanLexer lexer = new BooleanLexer(math);
-		BooleanParser parser = new BooleanParser(lexer);
-		BooleanInterpreter interpreter = new BooleanInterpreter(parser, ConsoleContext.INSTANCE);
-		System.out.println(interpreter.interpret());
+		String[] inputs = {real, real1, math, math1};
+		for (String input: inputs) {
+			BooleanLexer lexer = new BooleanLexer(input);
+			BooleanParser parser = new BooleanParser(lexer);
+			BooleanInterpreter interpreter = new BooleanInterpreter(parser, ConsoleContext.INSTANCE);
+			System.out.println(String.format("%s gives : %b", input, interpreter.interpret()));
+		}
 	}
 	
 	public static void main(String[] args) throws ParsingException, InterpretingException {
-//		testLexer();
-//		testParser();
+//		testLexer(input1);
+//		String[] ins = {input, input0, input1, real, real1, math, math1};
+//		for (String in : ins) testParser(in);
 		testInterpreter();
 	}
 }
