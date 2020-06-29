5ff6599ea929b7d7941661cdb88b507fd9887883
54754N4 <35555527+54754N4@users.noreply.github.com>
2020-06-29 10:39:43 +0700

    Removed full qualified name

--- src/test/parsing/TestArithmetics.java
+++ src/test/parsing/TestArithmetics.java
@@ -5,7 +5,7 @@ import interpreter.exceptions.ParsingException;
 
 public class TestArithmetics {
 	public static void main(String[] args) throws ParsingException {
-		interpreter.arithmetic.ArithmeticInterpreter interpreter = new ArithmeticInterpreter("2+3*4+PI+exp(ln(1))\n+2**(1+2)");
+		ArithmeticInterpreter interpreter = new ArithmeticInterpreter("2+3*4+PI+exp(ln(1))\n+2**(1+2)");
 		System.out.println(interpreter.interpret());
 	}
 }
